8e17b05194503c7360c12365222aae0f92d15b90
alicefirminger <alicefirminger@gmail.com>
2023-08-12 17:10:48 +0100

    hard coded so that move speed value shows in correct place. Need to get it so the totals will update when the row is clicked

--- main.js
+++ main.js
@@ -1,4 +1,55 @@
-console.log("hello, world")
+console.log("hello, world");
 
 // PLAN
 // Write a function that onclick adds the total columns selected and inputs them in a 'totals' column.
+// It should add up each 'section total' and then an overall weight total
+// Then it should have this formula 12 - overall weight total
+
+function createFormula() {
+	const basicsTotal = document.getElementById("basics-total").textContent;
+
+	const selectionsTotal =
+		document.getElementById("selections-total").textContent;
+	const lightTentValue = parseFloat(
+		document.getElementById("light-tent-value").textContent
+	);
+	const mountainTentValue = parseFloat(
+		document.getElementById("mountain-tent-value").textContent
+	);
+	const summerSleepingbagValue = parseFloat(
+		document.getElementById("summer-sleepingbag-value").textContent
+	);
+	const winterSleepingbagValue = parseFloat(
+		document.getElementById("winter-sleepingbag-value").textContent
+	);
+
+	const extrasTotal = document.getElementById("extras-total").textContent;
+	const waterValue = parseFloat(
+		document.getElementById("water-value").textContent
+	);
+	const extraFoodValue = parseFloat(
+		document.getElementById("extra-food-value").textContent
+	);
+	const accessoriesValue = parseFloat(
+		document.getElementById("accessories-value").textContent
+	);
+
+	let overallTotal = parseFloat(
+		document.getElementById("overall-total").textContent
+	);
+
+	overallTotal = basicsTotal + selectionsTotal + extrasTotal;
+
+	const moveSpeed = 12 - overallTotal;
+	const moveSpeedElement = document.createElement("h1");
+	const moveSpeedText = document.createTextNode(`${moveSpeed} + dice throw`);
+	moveSpeedElement.appendChild(moveSpeedText);
+	const moveSpeedTotal = document.getElementById("move-speed-total");
+	moveSpeedTotal.appendChild(moveSpeedElement);
+
+	console.log(selectionsTotal, "selectionsTotal");
+	console.log(extrasTotal, "extrasTotal");
+	console.log(overallTotal);
+	console.log(moveSpeed);
+}
+createFormula();
